f247c1545fe7afaedeab6ad533e1172f6e06d434
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-17 15:21:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name:, grade:,id:)
-    @name=name
-    @grade=grade
-    @id=id
-  end
+  attr_reader :id
 
   attr_accessor :name,:grade
 
