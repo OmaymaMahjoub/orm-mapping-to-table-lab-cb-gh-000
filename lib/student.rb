67251d5f7722ac42731d1ce175a311d6ff2dd7d4
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-17 15:22:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ class Student
   attr_reader :id
 
   attr_accessor :name,:grade
+  def initialize(name,grade)
+    @name=name
+    @grade=grade
+  end
 
   def self.create_table()
     sql="CREATE TABLE students(
