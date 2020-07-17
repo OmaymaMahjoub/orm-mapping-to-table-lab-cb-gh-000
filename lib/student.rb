75569b321e59c01c25d1d1a34d1d2dd564d992f0
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-17 15:19:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,23 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade)
+  def initialize(name, grade,id)
     @name=name
     @grade=grade
+    @id=id
   end
+
+  attr_accessor :name,:grade
+
+  def self.create_table(DB)
+    sql="CREATE TABLE students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    );"
+
+    DB[:conn].execute(sql)
+  end
+  
 
 end
